63a4767b25f0c152641caecaf196ec972c84e17a
Lucas Walter <wsacul@gmail.com>
2017-02-09 05:58:33 -0800

    The viewer doesn't keep up with a moderately sized point cloud, and seg faults when it exits.

--- rqt_pcl_visualizer/scripts/gen_pc2.py
+++ rqt_pcl_visualizer/scripts/gen_pc2.py
@@ -16,23 +16,12 @@ if __name__ == '__main__':
     pcl_pub = rospy.Publisher("/rqt_pcl_visualizer/point_cloud2", PointCloud2,
                               queue_size=2)
     rospy.sleep(1.0)
-    cloud_points = []
 
     fields = [PointField('x', 0, PointField.FLOAT32, 1),
               PointField('y', 4, PointField.FLOAT32, 1),
               PointField('z', 8, PointField.FLOAT32, 1),
               PointField('rgba', 12, PointField.UINT32, 1),
               ]
-
-    for theta in numpy.arange(0, math.pi * 2.0, 0.1):
-        for phi in numpy.arange(0, math.pi, 0.02):
-            r = math.sin(phi)
-            z = math.cos(phi)
-            x = r * math.cos(theta)
-            y = r * math.sin(theta)
-            r = int(255 * phi / math.pi)
-            rgba = 0x000000ff + (r << 16)
-            cloud_points.append([x, y, z, rgba])
 
     header = std_msgs.msg.Header()
 
@@ -41,20 +30,21 @@ if __name__ == '__main__':
         header.stamp = rospy.Time.now()
         header.frame_id = 'map'
 
+        cloud_points = []
         for theta in numpy.arange(0, math.pi * 2.0, 0.1):
             for phi in numpy.arange(0, math.pi, 0.02):
                 r = math.sin(phi)
                 z = math.cos(phi)
-                x = r * math.cos(theta)
+                x = r * math.cos(theta) + count * 0.1
                 y = r * math.sin(theta)
                 r = int(255 * phi / math.pi)
                 g = int(255 * theta / (2.0 * math.pi))
-                # rgba = 0x000000ff + (r << 24) + (g << 16) + (count << 8)
-                rgba = 0xffffffff & (0xff000000 + (r << 16) + (g << 8) + (count))
+                b = 0xff & int(count * 5)
+                rgba = 0xffffffff & (0xff000000 + (r << 16) + (g << 8) + (b))
                 cloud_points.append([x, y, z, rgba])
 
         pc2 = pcl2.create_cloud(header, fields, cloud_points)
         pcl_pub.publish(pc2)
-        rospy.sleep(0.2)
-        count += 5
-        count = count % 255
+        rospy.sleep(0.4)
+        count += 1
+        count = count
